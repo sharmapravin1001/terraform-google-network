ab5e5474620646d30a5ef331ce3d5ae4becb9b7a
sharmapravin1001 <80628061+sharmapravin1001@users.noreply.github.com>
2022-05-17 01:21:54 +0100

    Update route.tf

--- gke_provider/route.tf
+++ gke_provider/route.tf
@@ -1,14 +1,14 @@
-resource "google_compute_router" "pravin-router1" {
-  name                          = "pravin-router1"
-  network                       = google_compute_network.pravin-vpc.id
+resource "google_compute_router" "greenfield-router1" {
+  name                          = "greenfield-router1"
+  network                       = google_compute_network.greenfield-vpc.id
   bgp {
     asn = 64514
   }
 }
 
-resource "google_compute_firewall" "pravin-firewall" {
-  name    = "pravin-firewall"
-  network = google_compute_network.pravin-vpc.name
+resource "google_compute_firewall" "greenfield-firewall" {
+  name    = "greenfield-firewall"
+  network = google_compute_network.greenfield-vpc.name
 
   allow {
     protocol = "icmp"
@@ -25,8 +25,8 @@ resource "google_compute_firewall" "pravin-firewall" {
 /*
 resource "google_compute_router_nat" "nat" {
   name                               = "my-router-nat"
-  router                             = google_compute_router.pravin-router1.name
-  region                             = google_compute_router.pravin-router1.region
+  router                             = google_compute_router.greenfield-router1.name
+  region                             = google_compute_router.greenfield-router1.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
  
