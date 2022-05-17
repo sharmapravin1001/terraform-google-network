155cf836a64d97a8b4814812695a424f4af5be8d
sharmapravin1001 <80628061+sharmapravin1001@users.noreply.github.com>
2022-05-17 01:20:33 +0100

    Update vpc.tf

--- gke_provider/vpc.tf
+++ gke_provider/vpc.tf
@@ -1,21 +1,21 @@
-resource "google_compute_network" "pravin-vpc" {
+resource "google_compute_network" "greenfield-vpc" {
   project = "your-own-project-name-example-sandbox-123"
-  name = "pravin-vpc"
+  name = "greenfield-vpc"
   auto_create_subnetworks = false
 }
 
 
-resource "google_compute_subnetwork" "pravin-subnetwork1" {
-  name          = "pravin-subnetwork1"
+resource "google_compute_subnetwork" "greenfield-subnet1" {
+  name          = "greenfield-subnet1"
   ip_cidr_range = "10.0.1.0/24"
   region        = "europe-west2"
-  network       = google_compute_network.pravin-vpc.id
+  network       = google_compute_network.greenfield-vpc.id
 }
 
 
-resource "google_compute_subnetwork" "pravin-subnetwork2" {
-  name          = "pravin-subnetwork2"
+resource "google_compute_subnetwork" "greenfield-subnet2" {
+  name          = "greenfield-subnet2"
   ip_cidr_range = "10.0.2.0/24"
   region        = "europe-west2"
-  network       = google_compute_network.pravin-vpc.id
+  network       = google_compute_network.greenfield-vpc.id
 }
